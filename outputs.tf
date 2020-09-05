f817053853a671c896003683587540e4f662113e
pswitala <p.switala@pslab.eu>
2020-09-05 12:34:20 +0200

    Addedd header and footer for keybase pgp decrypt

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@ output "pscloud_iam_user" {
 }
 
 output "pscloud_access_key_encrypted" {
-  value = var.pscloud_create_access_keys ? aws_iam_access_key.pscloud-iam-user-acsess-key[0].encrypted_secret : ""
+  value = var.pscloud_create_access_keys ? join("\r\n", ["-----BEGIN PGP MESSAGE-----", "Comment: https://keybase.io/download", "Version: Keybase Go 5.5.0 (windows)", "", aws_iam_access_key.pscloud-iam-user-acsess-key[0].encrypted_secret, "-----END PGP MESSAGE-----"] )  : ""
 }
 
 output "pscloud_iam_user_console_password_encrypted" {
-  value = var.pscloud_create_access_keys ? aws_iam_user_login_profile.pscloud-iam-user-login-profile[0].encrypted_password : ""
+  value = var.pscloud_create_access_keys ? join("\r\n", ["-----BEGIN PGP MESSAGE-----", "Comment: https://keybase.io/download", "Version: Keybase Go 5.5.0 (windows)", "", aws_iam_user_login_profile.pscloud-iam-user-login-profile[0].encrypted_password, "-----END PGP MESSAGE-----"] ) : ""
 }
